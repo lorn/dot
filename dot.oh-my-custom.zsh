7dbee732bf75a0dac3175ea4c3ba19af83aa4e8e
Lindolfo 'Lorn' Rodrigues <lorn@lornlab.org>
2020-12-01 10:24:28 -0300

    Remove oh-my-zsh and spacemacs
    - Using doom-emacs now
    - Using vanilla oh-my-zsh now with my custom

---
+++ dot.oh-my-custom.zsh
@@ -0,0 +1,21 @@
+alias ssh='ssh -AC'
+
+alias ga='git add'
+alias gp='git push'
+alias gl='git log'
+alias gs='git status'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gm='git commit -m'
+alias gma='git commit -am'
+alias gb='git branch'
+alias gc='git checkout'
+alias gra='git remote add'
+alias grr='git remote rm'
+alias gpu='git pull'
+alias gcl='git clone'
+
+export PATH=/usr/local/bin:/usr/local/sbin:/home/lorn/.asdf/shims:/home/lorn/.asdf/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/lorn/.scripts/tools:/home/lorn/.scripts/statusbar:/home/lorn/.scripts/cron:/home/lorn/.scripts/i3cmds:/home/lorn/.scripts/:/home/lorn/.cargo/bin:/home/lorn/.local/bin:/home/lorn/.emacs.d/bin:$PATH
+
+export GIT_PATH="/home/lorn/src/"
+
--- install.sh
+++ install.sh
@@ -52,7 +52,7 @@ dot_clone_zsh()
   then
     (cd "$HOME/.oh-my-zsh" && $git_bin pull)
   else
-    $git_bin clone git://github.com/lorn/oh-my-zsh "$HOME/.oh-my-zsh"
+    $git_bin clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
   fi
 }
 
@@ -62,25 +62,28 @@ dot_install_dot()
   print_info "INSTALLING DOT FILES"
   $ln_bin -s -f -n "$HOME/.dotfiles/dot.gitconfig" "$HOME/.gitconfig"
   $ln_bin -s -f -n "$HOME/.dotfiles/Brewfile" "$HOME/.Brewfile"
-  $ln_bin -s -f -n "$HOME/.dotfiles/dot.spacemacs" "$HOME/.spacemacs"
   $ln_bin -s -f -n "$HOME/.dotfiles/dot.config" "$HOME/.config"
   $ln_bin -s -f -n "$HOME/.dotfiles/dot.scripts" "$HOME/.scripts"
-  $ln_bin -s -f -n "$HOME/.dotfiles/dot.xinitrc" "$HOME/.xinitrc"
-  $ln_bin -s -f -n "$HOME/.dotfiles/dot.profile" "$HOME/.profile"
-  $ln_bin -s -f -n "$HOME/.dotfiles/dot.profile" "$HOME/.xprofile"
-  $ln_bin -s -f -n "$HOME/.dotfiles/dot.profile" "$HOME/.zprofile"
   $ln_bin -s -f -n "$HOME/.dotfiles/dot.zshrc" "$HOME/.zshrc"
+  $ln_bin -s -f -n "$HOME/.dotfiles/dot.oh-my-custom.zsh" "$HOME/.oh-my-zsh/custom/custom-lorn.zsh"
+  $ln_bin -s -f -n "$HOME/.dotfiles/dot.doom.d" "$HOME/.doom.d"
 }
 
 
 dot_clone_spacemacs()
 {
-    print_info "INSTALLING SPACEMACS"
-    $git_bin clone https://github.com/syl20bnr/spacemacs "$HOME/.emacs.d"
+  print_info "CLONING DOOM-EMACS INTO $HOME/.emacs.d"
+  if [ -d "$HOME/.emacs.d" ]
+  then
+    print_info "Already cloned"
+  else
+    $git_bin clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+    ~/.emacs.d/bin/doom install
+  fi
 }
 
 check_binaries
 dot_clone_dot
-dot_install_dot
 dot_clone_zsh
+dot_install_dot
 dot_clone_spacemacs
