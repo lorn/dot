32783533063e9fea04ba9565990879b7f06529e7
Lindolfo 'Lorn' Rodrigues <lorn@lornlab.org>
2022-07-05 14:13:48 -0300

    new emacs/doom font

--- dot.doom.d/config.el
+++ dot.doom.d/config.el
@@ -19,7 +19,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monaco" :size 12))
+(setq doom-font (font-spec :family "MesloLGS NF" :size 15))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -42,6 +42,7 @@
 ;;(global-fasd-mode 1)
 
 (global-wakatime-mode 1)
+(setq wakatime-cli-path "/Users/lorn/.wakatime/wakatime-cli")
 
 (setq projectile-project-search-path '( "/Users/lorn/Documents/src/" ))
 
