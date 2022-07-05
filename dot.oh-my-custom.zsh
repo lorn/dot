322c2496998a0ea03046b90c482ddc7425b48979
Lindolfo 'Lorn' Rodrigues <lorn@lornlab.org>
2022-07-05 14:13:26 -0300

    config to compile ruby 2.5.7 with asdf

--- dot.oh-my-custom.zsh
+++ dot.oh-my-custom.zsh
@@ -15,7 +15,14 @@ alias grr='git remote rm'
 alias gpu='git pull'
 alias gcl='git clone'
 
-export PATH=/usr/local/bin:/usr/local/sbin:/Users/lorn/.asdf/shims:/Users/lorn/:/Users/lorn/.emacs.d/bin:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:/Users/lorn/.asdf/shims:/Users/lorn/.scripts/tools/:/Users/lorn/.emacs.d/bin:/opt/homebrew/bin:/opt/homebrew/lib:$PATH
 
 export GIT_PATH="/Users/lorn/Documents/src/"
+## ruby 2.5.7
 
+# openssl
+export PATH="/opt/homebrew/opt/openssl@1.1/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/openssl@1.1/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/openssl@1.1/include"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@1.1/lib/pkgconfig"
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/opt/homebrew/opt/openssl@1.1"
